1f8a966d61844673faaeb6f6eb1d320cc6238529
AlexandreGirolt <71321326+AlexandreGirolt@users.noreply.github.com>
2021-06-29 11:20:08 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,148 +3,20 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "Menu.h"
+#include "fromCsvToC.h"
+
 
 int main(){
-    printf("WELCOME TO THE FAMILY TREE PROJECT!!!\n\n");
-    
-    int good = 0;
-    
-    do{
-        showMenu();
 
-        char choice;
 
-        choice = getc(stdin);
+    struct TabPerson* tp = createEmptyTabPerson();
+    tp = fromCsvToC("bd_petite.csv");
 
-        switch (choice){
-            case '1':
-                printf("\nRead CSV file\n");
-                printf("------------------------\n");
-                printf("Enter CSV file name: ");
-                good++;
-                break;
-            case '2':
-                printf("\nGood Bye :)\n");
-                return 0;
-            default:
-                printf("\nWrong input :( !\n\n");
-                getc(stdin);
-                break;
-        }
-    
-        if(good != 0){
-            good = 0;
+    /*printf("Plus jeune personne: %s %s\n", tp->young->name, tp->young->familyName);
+    printf("Plus vieille personne: %s %s\n", tp->old->name, tp->old->familyName);
+    printf("Plus de naissance: %d\n", tp->valBestPlace);
+    printf("Lieu plus de naissance: %s\n", tp->bestPlace);*/
 
-            char filename[50];
-    
-            getc(stdin);
-            scanf("%s[^\n]", filename);
-    
-            struct TabPerson* tp = NULL;
-
-            if(tp == NULL){
-                printf("\nProblème lors de la lecture de la base de donné, soit celle-ci est vide, soit la lecture a eu un problème!\n");
-                exit(1);
-            }
-
-            printf("\nFile \"%s\" read successfully!\n", filename);
-            do{
-                showReadMenu();
-
-                char doChoice;
-                getc(stdin);
-        
-                doChoice = getc(stdin);
-
-                switch (doChoice){
-                    case '1':
-                        good = 1;
-                        break;
-                    case '2':
-                        good = 2;
-                        break;
-                    case '3':
-                        good = 3;
-                        break;
-                    case '4':
-                        good = 4;
-                        break;
-                    case '5':
-                        good = 5;
-                        break;
-                    case '6':
-                        good = 6;
-                        break;
-                    default:
-                        printf("\nWrong input :( !\n");
-                        break;
-                }
-    
-
-                if(good==5){
-                    do{
-                        showQueryMenu();
-
-                        char doChoice;
-                        getc(stdin);
-        
-                        doChoice = getc(stdin);
-
-                        switch (doChoice){
-                            case '1':
-                                firstBorn(tp);
-                                break;
-                            case '2':
-                                lastBorn(tp);
-                                break;
-                            case '3':
-                                printf("Enter the place: ");
-                                getc(stdin);
-
-                                char lieu[150];
-                                scanf("%s[^\n]", lieu);
-
-                                nbPeopleInRegion(tp, lieu);
-                                break;
-                            case '4':
-                                bestPlace(tp);
-                                break;
-                            case '5':
-                                printf("Enter the day: ");
-                               getc(stdin);
-
-                                int day, month;
-                                scanf("%d", &day);
-
-                                getc(stdin);
-                                scanf("%d", &month);
-
-                                calendar(tp, day, month);
-                                break;
-                            case '6':
-                                printf("\nNothing here\n");
-                                break;
-                            case '7':
-                                printf("In construction\n");
-                                break;
-                            case '8':
-                                good = 0;
-                                break;
-                            default:
-                                printf("\nWrong input :( !\n");
-                                break;
-                        }
-                    }while(good!=0);
-                    good = 0;
-                }
-
-            }while(good==0);
-            good = 0;
-            getc(stdin);
-        }
-
-    }while(good==0);
 
     return 0;
 }
